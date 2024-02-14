142f032e586d67d9338f51e250452331f4ea97ea
OwenJRJones <owenjrjones@gmail.com>
2024-02-14 16:07:36 -0400

    Fixed typo in Moncton

--- src/components/WeatherSection/WeatherSection.jsx
+++ src/components/WeatherSection/WeatherSection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import WeatherItem from '../WeatherItem/WeatherItem'
 import './WeatherSection.css'
 
-const locations = encodeURIComponent('Halifax,NS|Montcton,NB|Montreal,QC|Toronto,ON|Victoria,BC')
+const locations = encodeURIComponent('Halifax, NS|Moncton, NB|Montreal, QC|Toronto, ON|Victoria, BC')
 
 const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timelinemulti?/today?&locations=${locations}&unitGroup=metric&elements=datetime%2Cname%2Ctemp%2Cconditions%2Cicon&include=current&iconSet=icons2&key=79CPU88DC8B8ESJWV9U5SXS3B&contentType=json`
 
